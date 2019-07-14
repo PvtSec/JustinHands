a641a2a9073ed49d38c3c6e6a4f191ca55391522
PvtSec <akssofts@gmail.com>
2019-07-14 11:28:50 +0530

    Further Activities Added Animation

--- app/src/main/java/just/in/hands/Student_Dashboard.java
+++ app/src/main/java/just/in/hands/Student_Dashboard.java
@@ -1,17 +1,12 @@
 package just.in.hands;
 
 import android.app.ActivityOptions;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
-import android.transition.Fade;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -100,66 +95,66 @@ public class Student_Dashboard extends AppCompatActivity
                 @Override
                 public void onClick(View v)
                 {
-                    Intent update =new Intent(Student_Dashboard.this, Updates_Activity.class);
-                    View sharedView = findViewById(R.id.bell_icon);
-                    String transitionName = getString(R.string.dash_updates);
-                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, sharedView, transitionName);
-                    startActivity(update, transitionActivityOptions.toBundle());
+                    Intent updates =new Intent(Student_Dashboard.this, Updates_Activity.class);
+                    View update_view = findViewById(R.id.updates_icon);
+                    String updates_trans = getString(R.string.trans_updates);
+                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, update_view, updates_trans);
+                    startActivity(updates, transitionActivityOptions.toBundle());
                 }
             });
             notes.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v)
                 {
-                    Intent update =new Intent(Student_Dashboard.this, Updates_Activity.class);
-                    View sharedView = findViewById(R.id.bell_icon);
-                    String transitionName = getString(R.string.dash_updates);
-                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, sharedView, transitionName);
-                    startActivity(update, transitionActivityOptions.toBundle());
+                    Intent note =new Intent(Student_Dashboard.this, Notes_Activity.class);
+                    View notes_view = findViewById(R.id.notes_icon);
+                    String notes_trans = getString(R.string.trans_notes);
+                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, notes_view, notes_trans);
+                    startActivity(note, transitionActivityOptions.toBundle());
                 }
             });
             attendance.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v)
                 {
-                    Intent update =new Intent(Student_Dashboard.this, Updates_Activity.class);
-                    View sharedView = findViewById(R.id.bell_icon);
-                    String transitionName = getString(R.string.dash_updates);
-                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, sharedView, transitionName);
-                    startActivity(update, transitionActivityOptions.toBundle());
+                    Intent attendans =new Intent(Student_Dashboard.this, Attendance_Activity.class);
+                    View attendance_view = findViewById(R.id.attendance_icon);
+                    String attendance_trans = getString(R.string.trans_attendance);
+                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, attendance_view, attendance_trans);
+                    startActivity(attendans, transitionActivityOptions.toBundle());
                 }
             });
             internals.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v)
                 {
-                    Intent update =new Intent(Student_Dashboard.this, Updates_Activity.class);
-                    View sharedView = findViewById(R.id.bell_icon);
-                    String transitionName = getString(R.string.dash_updates);
-                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, sharedView, transitionName);
-                    startActivity(update, transitionActivityOptions.toBundle());
+                    Intent internal =new Intent(Student_Dashboard.this, Internals_Activity.class);
+                    View internals_view = findViewById(R.id.internals_icon);
+                    String internals_trans = getString(R.string.trans_internals);
+                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, internals_view, internals_trans);
+                    startActivity(internal, transitionActivityOptions.toBundle());
                 }
             });
             projects.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v)
                 {
-                    Intent update =new Intent(Student_Dashboard.this, Updates_Activity.class);
-                    View sharedView = findViewById(R.id.bell_icon);
-                    String transitionName = getString(R.string.dash_updates);
-                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, sharedView, transitionName);
-                    startActivity(update, transitionActivityOptions.toBundle());
+                    Intent project =new Intent(Student_Dashboard.this, Projects_Activity.class);
+                    View projects_view = findViewById(R.id.projects_icon);
+                    String project_trans = getString(R.string.trans_projects);
+                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, projects_view, project_trans);
+                    startActivity(project, transitionActivityOptions.toBundle());
                 }
             });
             helpdesk.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v)
                 {
-                    Intent update =new Intent(Student_Dashboard.this, Updates_Activity.class);
-                    View sharedView = findViewById(R.id.bell_icon);
-                    String transitionName = getString(R.string.dash_updates);
-                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, sharedView, transitionName);
-                    startActivity(update, transitionActivityOptions.toBundle());
+                    Intent helpdesks =new Intent(Student_Dashboard.this, Helpdesk_Activity.class);
+                    View helpdesk_view = findViewById(R.id.helpdesk_icon);
+                    String helpdesk_trans = getString(R.string.trans_helpdesk);
+                    ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(Student_Dashboard.this, helpdesk_view, helpdesk_trans);
+                    startActivity(helpdesks, transitionActivityOptions.toBundle());
                 }
             });
 
